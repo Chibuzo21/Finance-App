16ebff870eafb01b6750783d5564f9ed9d96f24c
chibuzo <uchibuzo1@gmail.com>
2025-09-15 09:04:58 +0100

    new

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Loginpage() {
   return (
-    <div className='mx-auto w-full flex flex-col space-y-6 justify-center sm:w-[350px] py-40'>
+    <div className='mx-auto w-full flex flex-col space-y-6 justify-center sm:w-[350px] p-40'>
       <div className='flex flex-col space-y-3 text-center'>
         <h1 className='text-2xl font-semibold'>Welcome back</h1>
         <p className=' dark:text-[#74e2c8]'>Enter your details to sign in</p>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from "./components/signupform";
 
 export default function Signup() {
   return (
-    <div className='mx-auto flex flex-col space-y-4 justify-center w-[450px] py-32'>
+    <div className='mx-auto w-full flex flex-col space-y-4 justify-center sm:w-[450px] p-32'>
       <div>
         <h1 className='text-4xl font-medium mb-3'>Create An Account</h1>
         <p>Please enter your details below to continue</p>
