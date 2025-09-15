8d1283001b3a9c0239cf3b8ef4941acc418ae608
chibuzo <uchibuzo1@gmail.com>
2025-09-15 14:39:33 +0100

    restored styles of dashboard

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from "./components/signupform";
 
 export default function Signup() {
   return (
-    <div className='mx-auto w-full flex flex-col space-y-4 justify-center sm:w-[450px] py-32 px-9'>
+    <div className='mx-auto w-full flex flex-col space-y-4 justify-center sm:w-[450px] py-32 px-9 sm:px-0'>
       <div>
         <h1 className='text-4xl font-medium mb-3'>Create An Account</h1>
         <p>Please enter your details below to continue</p>
